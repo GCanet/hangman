9b7a7827808eb915b7b518033cfa2249d4f0d68a
Gerard <kanetpwnz@gmail.com>
2023-08-01 14:13:47 +0200

    added functionalities

--- hangman.rb
+++ hangman.rb
@@ -10,16 +10,43 @@ class Hangman
 
   def playinicial
     loop do
-      puts 'Para adivinar palabras pulsa A, para salir pulsa S (a/s):'
-      awnser = gets.chomp.downcase
-      if awnser == 'a'
+      puts 'Para adivinar palabras pulsa A, para cargar una partida pulsa C, para salir pulsa S (a/s):'
+      answer = gets.chomp.downcase
+
+      case answer
+      when 'a'
+        vidas = 6
+        puts '----------------------'
+        puts '|      hangman       |'
+        puts '----------------------'
         puts 'Empieza el juego, tú adivinas!'
         @breaker.partida
-      elsif awnser == 's'
+      when 'c'
+        puts 'Cargando partida...'
+        load_game
+      when 's'
         puts 'Cerrando juego.'
         exit
+      else
+        puts 'Entrada inválida. Por favor, pulsa A, C o S.'
       end
     end
+  end
+
+  def save_game
+    yaml = YAML.dump(self)
+    game_file = GameFile.new('saved.yaml')
+    game_file.write(yaml)
+  end
+
+  def load_game
+    game_file = GameFile.new('saved.yaml')
+    yaml = game_file.read
+    loaded_game = YAML.load(yaml)
+    self.guess = loaded_game.guess
+    self.solucion = loaded_game.solucion
+    self.vidas = loaded_game.vidas
+    @breaker.partida
   end
 end
 
@@ -40,11 +67,27 @@ class Breaker
     fname = 'dictionary.txt'
     @hangman.solucion = File.readlines(fname).select { |word| word.length > 5 && word.length < 12 }.sample.chomp.upcase
     puts "La máquina ya tiene palabra, su longitud es: #{hangman.solucion.length}."
+    @hangman.guess = '_' * @hangman.solucion.length
     puts @hangman.solucion
   end
 
   def user_guess
-    puts 'Introduzca su conjetura:'
+    puts 'Si quieres guardar y salir pulsa S, si quieres continuar pulsa C (s/c)'
+    answer = gets.chomp.downcase
+
+    case answer
+    when 'c'
+      introducir_letra
+    when 's'
+      @hangman.save_game
+      exit
+    else
+      puts 'Entrada inválida. Por favor, pulsa S o N.'
+    end
+  end
+
+  def introducir_letra
+    puts 'Introduzca una letra:'
     @hangman.guess = gets.chomp.upcase.split('')
     comprobacion
   end
@@ -144,9 +187,27 @@ class Breaker
     end
 
     if vidas == 0
-      puts 'El usuario ha perdido!'
+      puts 'Game over! Te has quedado sin vidas!!'
       @hangman.playinicial
     end
+  end
+end
+
+class GameFile
+  attr_accessor :filename
+
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def write(data)
+    File.open(@filename, 'w') do |file|
+      file.write(data)
+    end
+  end
+
+  def read
+    File.read(@filename)
   end
 end
 
