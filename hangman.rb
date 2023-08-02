8baf246e3342ed63b48f31085574a9ce25a8b07a
Gerard <kanetpwnz@gmail.com>
2023-08-03 00:24:40 +0200

    language support

--- hangman.rb
+++ hangman.rb
@@ -18,15 +18,15 @@ class Breaker
     fname = 'dictionary.txt'
     if @hangman.solucion.empty?
       @hangman.solucion = File.readlines(fname).select { |word| word.length > 5 && word.length < 12 }.sample.chomp.upcase
-      puts "La máquina ya tiene palabra, su longitud es: #{hangman.solucion.length}."
+      puts "Machine has found a word to guess, length: #{@hangman.solucion.length}."
       @hangman.guess = '_' * @hangman.solucion.length
     else
-      puts "La máquina ya tiene una palabra guardada: #{@hangman.solucion}"
+      puts @hangman.mensajes[:palabr_guard]
     end
   end
 
   def user_guess
-    puts 'GUARDAR Y SALIR? (y/n)'
+    puts @hangman.mensajes[:guard_salir]
     answer = gets.chomp.downcase
 
     case answer
@@ -37,22 +37,22 @@ class Breaker
       puts '..'
       puts '...'
       puts '....'
-      puts '.....Guardando partida'
+      puts @hangman.mensajes[:guard_part]
       @hangman.save_game
       exit
     else
-      puts 'Letra erronea, introduzca (Y)es o (N)o'
+      puts @hangman.mensajes[:entr_anvalid]
       user_guess
     end
   end
 
   def introducir_letra
-    puts "Letras utilizadas: #{@hangman.used_letters}"
-    puts 'Introduzca una letra:'
+    puts @hangman.mensajes[:letras_utilizadas] + " #{@hangman.used_letters}"
+    puts @hangman.mensajes[:introducir_letra]
     letter = gets.chomp.upcase
 
     if @hangman.used_letters.include?(letter)
-      puts 'Esta letra ya ha sido utilizada.'
+      puts @hangman.mensajes[:letra_repetida]
     else
       @hangman.used_letters << letter
       @hangman.solucion.chars.each_with_index do |lett, index|
@@ -62,13 +62,17 @@ class Breaker
       end
     end
     if @hangman.solucion == @hangman.guess
-      puts '¡Tenemos un ganador!'
+      dibujo_hangman(@hangman.vidas)
+      puts "*[ #{@hangman.guess} ]*"
+      puts @hangman.mensajes[:ganador]
       @hangman.playinicial
     elsif @hangman.solucion.chars.include?(letter)
-      puts '¡Adivinaste una letra correctamente!'
+      dibujo_hangman(@hangman.vidas)
+      puts "*[ #{@hangman.guess} ]*"
+      puts @hangman.mensajes[:adivinaste_letra]
       user_guess
     else
-      puts 'El usuario pierde una vida.'
+      puts @hangman.mensajes[:pierde_vida]
       vida_menos
     end
   end
@@ -159,14 +163,14 @@ class Breaker
     end
 
     if vidas.zero?
-      puts 'Game over! Te has quedado sin vidas!!'
+      puts @hangman.mensajes[:gmee_ovr]
       @hangman.playinicial
     end
   end
 end
 
 class Hangman
-  attr_accessor :guess, :solucion, :vidas, :used_letters
+  attr_accessor :guess, :solucion, :vidas, :used_letters, :mensajes
 
   def initialize
     @breaker = Breaker.new(self)
@@ -174,11 +178,64 @@ class Hangman
     @solucion = ''
     @vidas = 6
     @used_letters = []
+    @mensajes = {}
+  end
+
+  def lang_inicial
+    puts '1 = English / 2 = Spanish'
+    awnser = gets.chomp.downcase
+
+    case awnser
+    when '1'
+      mensajes = {
+        letras_utilizadas: 'Used letters:',
+        introducir_letra: 'Enter a letter:',
+        letra_repetida: 'This letter has already been used.',
+        ganador: 'We have a winner!',
+        adivinaste_letra: 'You guessed a letter correctly!',
+        pierde_vida: 'The user loses a life.',
+        gmee_ovr: 'Game Over! You have no more lifes!',
+        mensaje_inicial: 'PLAY = A, LOAD GAME = C, EXIT = S:',
+        empieza_juego: 'Game start, you are guessing!',
+        no_partida: 'We did not find any saved game',
+        load_game: '.....Loading Game',
+        closing_game: 'Closing game',
+        entr_invalida: 'Incorrect awnser. Please, push A, C or S.',
+        guard_salir: 'SAVE AND EXIT? (y/n)',
+        guard_part: '.....Saving game',
+        entr_anvalid: 'Incorrect awnser. Please, push (Y)es o (N)o',
+        palabr_guard: 'The game already have a word to guess.'
+      }
+      playinicial
+    when '2'
+      mensajes = {
+        letras_utilizadas: 'Letras utilizadas:',
+        introducir_letra: 'Introduzca una letra:',
+        letra_repetida: 'Esta letra ya ha sido utilizada.',
+        ganador: '¡Tenemos un ganador!',
+        adivinaste_letra: '¡Adivinaste una letra correctamente!',
+        pierde_vida: 'El usuario pierde una vida.',
+        gmee_ovr: 'Game over! Te has quedado sin vidas!!',
+        mensaje_inicial: 'ADIVINAR = A, CARGAR PARTIDA = C, SALIR = S:',
+        empieza_juego: 'Empieza el juego, tú adivinas!',
+        no_partida: 'No se encontró ninguna partida guardada.',
+        load_game: '.....Cargando partida',
+        closing_game: 'Cerrando juego.',
+        entr_invalida: 'Entrada inválida. Por favor, pulsa A, C o S.',
+        guard_salir: 'GUARDAR Y SALIR? (y/n)',
+        guard_part: '.....Guardando partida',
+        entr_anvalid: 'Letra erronea, introduzca (Y)es o (N)o',
+        palabr_guard: 'La máquina ya tiene una palabra guardada.'
+      }
+      playinicial
+    else
+      puts 'Please write 1 or 2'
+    end
   end
 
   def playinicial
     loop do
-      puts 'ADIVINAR = A, LOAD GAME = C, EXIT = S:'
+      puts mensajes[:mensaje_inicial]
       answer = gets.chomp.downcase
 
       case answer
@@ -187,20 +244,20 @@ class Hangman
         puts '----------------------'
         puts '|      hangman       |'
         puts '----------------------'
-        puts 'Empieza el juego, tú adivinas!'
+        puts mensajes[:empieza_juego]
         @breaker.partida
       when 'c'
         puts '.'
         puts '..'
         puts '...'
         puts '....'
-        puts '.....Cargando partida'
+        puts mensajes[:load_game]
         load_game
       when 's'
-        puts 'Cerrando juego.'
+        puts mensajes[:closing_game]
         exit
       else
-        puts 'Entrada inválida. Por favor, pulsa A, C o S.'
+        puts mensajes[:entr_invalida]
       end
     end
   end
@@ -210,7 +267,8 @@ class Hangman
       guess: @guess,
       solucion: @solucion,
       vidas: @vidas,
-      used_letters: @used_letters
+      used_letters: @used_letters,
+      mensajes: @mensajes
     }
     File.open('saved.json', 'w') do |file|
       file.write(JSON.dump(game_data))
@@ -224,12 +282,13 @@ class Hangman
       @solucion = game_data['solucion']
       @vidas = game_data['vidas']
       @used_letters = game_data['used_letters']
+      @mensajes = game_data['mensajes']
       @breaker.partida
     else
-      puts 'No se encontró ninguna partida guardada.'
+      puts mensajes[:no_partida]
     end
   end
 end
 
 new_game = Hangman.new
-new_game.playinicial
+new_game.lang_inicial
